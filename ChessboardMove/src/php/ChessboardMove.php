ab86bccbb95b6b7b5714c903833294296aa7c33c
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-09-07 08:13:14 -0700

    Add new exercises

---
+++ ChessboardMove/src/php/ChessboardMove.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ *  "You have an infinite chessboard, and a knight. The knight starts 
+ *  at [0, 0] and can move [like a knight moves, skipped for brevity]. 
+ *  Given two coordinates on the board, return the least amount of moves 
+ *  the knight has to make to get to that position". 
+ */
