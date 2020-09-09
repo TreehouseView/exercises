e053e48f191ad9ebc08347f23c3f6f25c15eab0d
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-09-09 16:56:38 -0700

    Add new exercise "PermCheck"

---
+++ PermCheck/src/php/PermCheck.php
@@ -0,0 +1,19 @@
+function solution($A) {
+    $isPermutation = 1;
+    $missing = 1;
+    
+    // Create index
+    $index = array_flip($A);
+    
+    // Find missing number
+    while (isset($index[$missing])) {
+        $missing++;
+    }
+    if ($missing != count($A) + 1) {
+        $isPermutation = 0;
+    }
+    return $isPermutation;
+}
+
+var_dump(solution([4,1,3,2,5]) . ' = 1');
+var_dump(solution(1,2,3,5) . ' = 0');
