0a8783c3317ec911fd5cee9afd6af47d290b57ff
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-09-09 19:17:16 -0700

    Updates

--- CountDiv/src/php/CountDiv.php
+++ CountDiv/src/php/CountDiv.php
@@ -7,11 +7,11 @@ function solution($A, $B, $K) {
     return $divisibleCount;
 }
 
-var_dump(solution(11,345,17)) . " = 20";
-var_dump(solution(1,1,11)) . " = 0";
-var_dump(solution(0,0,11)) . " =   1";
-var_dump(solution(5,10,5)) . " = 2";
-var_dump(solution(10,10,5)) . " = 1";
-var_dump(solution(10,11,5)) . " = 1";
-var_dump(solution(10,10,7)) . " = 0";
-var_dump(solution(10,10,20)) . " = 0";
+var_dump(solution(11,345,17) . " = 20");
+var_dump(solution(1,1,11) . " = 0");
+var_dump(solution(0,0,11) . " =   1");
+var_dump(solution(5,10,5) . " = 2");
+var_dump(solution(10,10,5) . " = 1");
+var_dump(solution(10,11,5) . " = 1");
+var_dump(solution(10,10,7) . " = 0");
+var_dump(solution(10,10,20) . " = 0");
