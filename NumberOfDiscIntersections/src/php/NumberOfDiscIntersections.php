3a8d980a9e0d927b42b0469299dab67ffd1d636d
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-09-10 08:38:29 -0700

    Add new exercise "NumberOfDiscIntersections"

---
+++ NumberOfDiscIntersections/src/php/NumberOfDiscIntersections.php
@@ -0,0 +1,33 @@
+<?php
+
+// you can write to stdout for debugging purposes, e.g.
+// print "this is a debug message\n";
+
+function solution($A) {
+    $pairs = [];
+    $count = 0;
+    for ($J = 0,$total = count($A);$J<=$total-2;$J++) {
+        for ($K = $J+1;$K<=$total-1;$K++) {
+            if ($K-$J <= $A[$K] + $A[$J]) {
+                $pairs[] = [$J, $K];
+                $count++;
+                if ($count > 10000000) {
+                    $count = -1;
+                    break;
+                }
+            }
+        }
+    }
+    
+   display($pairs);
+
+    return $count;
+}
+
+function display($array) {
+    foreach ($array as $value) {
+        echo "{$value[0]}, {$value[1]}\n";
+    }
+}
+
+
