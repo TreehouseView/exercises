643c33c5909ad4aaceaa46174dd99337803f5905
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-09-18 00:13:31 -0700

    Refactored to search backwards

--- FindWords/src/nodejs/FindWords.js
+++ FindWords/src/nodejs/FindWords.js
@@ -9,85 +9,101 @@ function solution(longStrings, wordFile) {
         data.toString().split("\r\n").forEach((value) => {
             wordIndex.set(value);
         });
-        let prefixIndex = new Map();
+        let suffixIndex = new Map();
         wordIndex.forEach((value, key) => {
             if (key.length > 1) {
-                let prefix = key[0] + key[1];
-                if (prefixIndex.has(prefix)) {
-                    let newValue = prefixIndex.get(prefix);
+                let total = key.length;
+                let suffix = key[total-2] + key[total-1];
+                if (suffixIndex.has(suffix)) {
+                    let newValue = suffixIndex.get(suffix);
                     newValue.push(key);
-                    prefixIndex.set(prefix, newValue);
+                    suffixIndex.set(suffix, newValue);
                 }
                 else {
-                    prefixIndex.set(prefix, [key]);
+                    suffixIndex.set(suffix, [key]);
                 }
             }
         });
         longStrings.forEach((longString) => {
-            console.log(parseString(longString, wordIndex, prefixIndex));
+            console.log(parseString(longString, wordIndex, suffixIndex));
         });
     });
 }
 
-function parseString(longString, wordIndex, prefixIndex) {
+function parseString(longString, wordIndex, suffixIndex) {
     let strL = longString.length;
     let lastPos = strL-1;
     let parsedString = '';
-    let x = 0;
-    while (x < lastPos) {
-        let prefix = [x,x+1];
+    let x = lastPos;
+    let wordLastAdded = true;
+    while (x > 0) {
+        let suffix = [x-1,x];
         let myCache = new Map();
-        let wordFound = findViaPrefix(prefix, longString, wordIndex, prefixIndex,myCache);
+        let wordFound = findViaPrefix(suffix, longString, wordIndex, suffixIndex,myCache);
         if (wordFound !== null) {
-            parsedString += ' ' + wordFound;
-            x += wordFound.length;
+            //console.log('Found word: ', wordFound);
+            parsedString = wordFound + ' ' + parsedString;
+            x -= wordFound.length;
+            wordLastAdded = true;
         }
         else {
-            parsedString += longString[x];
-            x++;
-            if (x === lastPos) {
-                if (wordIndex.has(longString[x])) {
-                    parsedString += ' ' + longString[x];
-                }
-                else {
-                    parsedString += longString[x];
-                }
+            if (wordLastAdded) {parsedString = ' ' + parsedString;}
+            parsedString = longString[x] + parsedString;
+            x--;
+            wordLastAdded = false;
+        }
+        if (x === 0) {
+            if (wordIndex.has(longString[x])) {
+                parsedString = longString[x] + ' ' + parsedString;
+            }
+            else {
+                if (wordLastAdded) {parsedString = ' ' + parsedString;}
+                parsedString = longString[x] +  parsedString;
             }
         }
     }
     return parsedString;
 }
 
-function findViaPrefix(prefix, longString, wordIndex, prefixIndex, myCache) {
-    let prefixValue = longString[prefix[0]] + longString[prefix[1]];
+function findViaPrefix(suffix, longString, wordIndex, suffixIndex, myCache) {
+    let suffixValue = longString[suffix[0]] + longString[suffix[1]];
     let match = null;
-    if (prefixIndex.has(prefixValue)) {
-        if (!myCache.has(prefixValue)) {
-            let sorted = prefixIndex.get(prefixValue);
+    if (suffixIndex.has(suffixValue)) {
+        if (!myCache.has(suffixValue)) {
+            let sorted = suffixIndex.get(suffixValue);
             sorted.sort((val1, val2) => {
                 if (val1.length === val2.length) {return 0}
                 else if (val1.length > val2.length) {return -1}
                 else {return 1}
             });
-            myCache.set(prefixValue, sorted);
+            myCache.set(suffixValue, sorted);
         }
-        let candidates = myCache.get(prefixValue);
+        let candidates = myCache.get(suffixValue);
+        //console.log(suffixValue, candidates.length, candidates[0]);
         let total = candidates.length;
         for (let x=0; x<total; x++) {
-            if (candidates[x] === longString.slice(prefix[0], candidates[x].length + prefix[0])) {
+            if (candidates[x].length > suffix[1] + 1) {continue;}
+            //console.log(candidates[x], longString.slice(suffix[1]+1 - candidates[x].length, suffix[1]+1));
+            if (candidates[x] === longString.slice(suffix[1]+1 - candidates[x].length, suffix[1]+1)) {
                 match = candidates[x];
                 break;
             }
         }
         if (match === null
-          && wordIndex.has(longString[prefix[0]])) {
-            match = longString[prefix[0]];
+          && wordIndex.has(longString[suffix[1]])) {
+            match = longString[suffix[1]];
         }
-
     }
     return match;
 }
 
 
-solution(['thequickbrownfoxjumpsoverthelazydog',
-'Iamking','Iliketoeatalotofpie'], __dirname + '/words_alpha.txt');
+solution([
+    'thequickbrownfoxjumpsoverthelazydog'
+    ,'iamking'
+    ,'iliketoeatalotofpie'
+    ,'mypoolneedsalotofcleaning'
+    ,'ilovetoplaytheguitarsomuchicouldfart'
+    ,'iliketoworkonmygardenontheweekends'
+    ,'ibuiltmyownplayground'
+], __dirname + '/words_alpha.txt');
