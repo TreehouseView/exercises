dc942db8c4dd67931fc363e2d8533c77081dc3fe
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-10-05 23:16:36 -0700

    New use cases

--- Dominator/src/php/Dominator.php
+++ Dominator/src/php/Dominator.php
@@ -28,3 +28,7 @@ function solution($A) {
 }
 
 var_dump(solution([3,4,3,2,3,-1,3,3]) . ' = 0');
+var_dump(solution([5,5,5,5,5,2,5]) . ' = 0');
+var_dump(solution([3,1,33,7,33,20,42,33,33,17
+    ,33,33,90,33,27,33,33]) . ' = 2');
+var_dump(solution([90,91,92,93,94,95,96,97]) . ' = -1');
